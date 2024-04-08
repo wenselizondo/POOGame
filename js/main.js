0338400371f2123b53be34db2049eb0e432e6a58
Wen S Elizondo <101236583+wenselizondo@users.noreply.github.com>
2024-04-07 23:20:56 -0600

    Testing
    Testing

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+Function bla bla bla
+*/
+
+import { Person } from './persona.js';
+window.onload = init;
+
+function init() {
+    console.log('init');
+    let person = new Person('esteban');
+};
